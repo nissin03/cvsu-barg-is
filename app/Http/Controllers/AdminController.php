33a0353bb94fe6a3f5127115f6cfba8a6c7e893f
johnonly10 <johnortigas12@gmail.com>
2025-12-01 14:33:57 +0800

    chore: FROM Orders to FROM orders

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -113,7 +113,7 @@ class AdminController extends Controller
                     SUM(IF(status='reserved', total, 0)) AS TotalReservedAmount,
                     SUM(IF(status='pickedup', total, 0)) AS TotalPickedUpAmount,
                     SUM(IF(status='canceled', total, 0)) AS TotalCanceledAmount
-                FROM Orders
+                FROM orders
                 WHERE YEAR(created_at) = ?
                 GROUP BY MONTH(created_at)
             ) D ON D.MonthNo = M.id
@@ -172,7 +172,7 @@ class AdminController extends Controller
                     SUM(IF(status = 'reserved', total, 0)) AS TotalReservedAmount,
                     SUM(IF(status = 'pickedup', total, 0)) AS TotalPickedUpAmount,
                     SUM(IF(status = 'canceled', total, 0)) AS TotalCanceledAmount
-                FROM Orders
+                FROM orders
                 WHERE created_at BETWEEN ? AND ?
             ", [$startOfWeek, $endOfWeek]);
 
@@ -241,7 +241,7 @@ class AdminController extends Controller
                 SUM(IF(status = 'reserved', total, 0)) AS TotalReservedAmount,
                 SUM(IF(status = 'pickedup', total, 0)) AS TotalPickedUpAmount,
                 SUM(IF(status = 'canceled', total, 0)) AS TotalCanceledAmount
-            FROM Orders
+            FROM orders
             WHERE created_at BETWEEN ? AND ?
             GROUP BY DAYOFWEEK(created_at), DAYNAME(created_at)
             ORDER BY DayNo
@@ -294,7 +294,7 @@ class AdminController extends Controller
                 SUM(IF(status = 'reserved', 1, 0)) AS TotalReserved,
                 SUM(IF(status = 'pickedup', 1, 0)) AS TotalPickedUp,
                 SUM(IF(status = 'canceled', 1, 0)) AS TotalCanceled
-            FROM Orders
+            FROM orders
             WHERE YEAR(created_at) = ?
         ";
 
